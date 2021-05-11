9a9360267dc0ef34be0462bde859e391cc21ebae
Bogenbai Bayzharassov <thatoranzhevyy@gmail.com>
2021-05-12 03:31:45 +0600

    add custom cover

--- layouts/cover.vue
+++ layouts/cover.vue
@@ -1,7 +1,32 @@
+<script setup>
+import {defineProps} from 'vue'
+
+const props = defineProps({
+  company: {
+    type: String,
+  },
+  date: {
+    type: String,
+  },
+})
+</script>
 <template>
   <div class="slidev-layout cover">
+    <div v-if="company" class="company-block">
+      <span class="logo"></span>
+      <span class="pl-6 company">
+        {{ company }}
+      </span>
+    </div>
     <div class="my-auto w-full">
-      <slot />
+      <div class="block">
+        <slot/>
+        <div v-if="date" class="pt-24">
+          <span class="date">
+            {{ date }}
+          </span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
