32494f1df59996dd6ed58bceb6a40a7f949fb562
thatoranzhevyy <thatoranzhevyy@gmail.com>
2021-05-12 10:52:10 +0600

    update

--- windi.config.ts
+++ windi.config.ts
@@ -19,8 +19,7 @@ export default mergeWindicssConfig(
       extend: {
         // fonts can be replaced here, remember to update the web font links in `index.html`
         fontFamily: {
-          sans: '"Nunito Sans", ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"',
-          mono: '"Fira Code", monospace',
+          sans: '"Work Sans", sans-serif'
         },
       },
     },
